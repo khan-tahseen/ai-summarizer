955382f750d342b8fa379f4d3d15f16c1a27de53
khan-tahseen <tasheenkhanit@gmail.com>
2024-01-08 23:07:39 +0530

    working on articel summary

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -103,7 +103,11 @@ const Demo = () => {
               <h2 className="font-satoshi font-bold text-gray-600 text-xl">
                 Article <span className="blue_gradient">Summary</span>
               </h2>
-              <p className='summary_box'>{article.summary}</p>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {article.summary}
+                </p>
+              </div>
             </div>
           )
         )}
